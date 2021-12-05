84a381afe2779a0f6853bdf36c94052cfd4dfc8b
Altun0 <91114236+Altun0@users.noreply.github.com>
2021-12-05 17:51:40 +0400

    new commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let namee = document.querySelector("#firstname")
+let btn=document.querySelector(".btn")
+let lastname = document.querySelector("#lastname")
+let password = document.querySelector("#password")
+
+
+
+const form = document.getElementById("form")
+
+
+if (namee.value=="") {
+    alert ("please your add name ")
+}
+
+if (lastname.value=="") {
+    alert ("please your add lastname ")
+}
+
+if (password.value=="") {
+    alert ("please your add password ")
+}
+
+
+
+
+
